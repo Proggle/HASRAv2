538b6636d979f4f683672fd369d1627a99289cf1
gavin <2518738740@qq.com>
2020-08-27 13:14:54 -0400

    made prepro script for mirrored video

--- src/client/find_mirror_angle.py
+++ src/client/find_mirror_angle.py
@@ -5,16 +5,18 @@ This problem can be solved in 2D space (x, z) because when y = 0 the angle the m
 note: this assumes mirror thickness == 0, you can adjust degrees of overcoverage to account for mirror thickness
 '''
 
-edge_a = 10 # distance from camera to where z = 0
-edge_b = 4 # distance from far side of the slot where the mouse reaches out from to the inner edge of the mirror
+edge_a = 5 # distance from camera to where z = 0
+edge_b = 0.5 # distance from far side of the slot where the mouse reaches out from to the inner edge of the mirror
 degrees_of_overcoverage = 5 # how many degrees past z=0 do you want?
 
 # this func returns the angle of the mirror relative to the z-axis
 def get_angle(edge_a, edge_b, degrees_of_overcoverage):
     edge_x = edge_b * math.tan(math.radians(degrees_of_overcoverage))
     edge_e = (edge_a + edge_x)/2 - edge_x
-    theta_C = math.degrees(math.atan(edge_e/edge_b))
+    print(edge_e)
+    theta_C = math.degrees(math.atan(edge_b/edge_e))
+    print(theta_C)
+
     theta_F = 90 - theta_C
-    theta_D = 90 - theta_F
-    return theta_D
+    return theta_F
 print(get_angle(edge_a, edge_b, degrees_of_overcoverage))
