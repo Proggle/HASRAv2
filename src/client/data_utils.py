d88bf3afa035933598d4d54e532d93c8dc0b2301
SilasiLab <35037531+SilasiLab@users.noreply.github.com>
2020-11-25 14:21:39 -0500

    added calibration photo selector
    the new class also handles for handedness of the animals, but might be inaccurate due to asymmetry of the left and right mirrors

--- src/client/data_utils.py
+++ src/client/data_utils.py
@@ -698,6 +698,145 @@ class thirds_w_handedness:
             out3.release()
             cv2.destroyAllWindows()
 
+            
+'''
+What does this class do?
+1. loads video input in avi form from single camera 3-mirrored setup and flips frames based on reading from handedness.txt
+2. spatially splits the video in 3 and pseudo-randomly samples frames between start_time and end_time.
+   You can change the factor by which the sampling is fixed to prevent consecutive sampling in
+   the gen_sample method.
+3. sampled frames are outputted to [your project dir]/calibration_images with the naming convention; camera-[cam number]-[frame number].jpg
+
+
+note1: filenames from camera may contain brackets and other characters, failing to handle for
+       this will cause the load_vid method to throw an OSErr
+note2: if you want to use >99 calibration images (num_cal_imgs > 99), please change zfill(2)-> zfill(3)
+       in the load_vid method. I don't know if DeepLabCut can handle for this.
+note3: there must be at least num_cal_imgs*3 frames within (end_time - start_time) or
+       gen_sample will throw, that is a bare minimum though and should not be used
+note4: make sure the lines at the bottom that instantiate this class and run its method are not commented out
+
+params:
+vid_name = r'2020-07-21_(09-02-58)_00782B1B4A02_16_2022.avi'
+project_name = 'stereo_cam_test1-silasi_lab-2020-07-21-3d'
+start_time = 6
+end_time = 13
+
+TODO:
+'''
+
+project_name = 'stereo_cam_test1-silasi_lab-2020-07-21-3d'
+vid_name = r'2020-07-23_(11-45-24)_00783A32F484_16_2015.avi'
+start_time = 11
+end_time = 29
+
+seed(2020)
+
+
+class VidLoader_w_handedness:
+    def __init__(self, project_path, vid_name, start_time, end_time, num_cal_imgs=200):
+        self.project_path = project_path
+        self.vid_name = vid_name
+        self.start_time = start_time
+        self.end_time = end_time
+        self.num_cal_imgs = num_cal_imgs
+        self.handedness_lookup = self.get_handedness()
+
+    def get_handedness(self):
+        handedness_lookup = defaultdict(str)
+        try:
+            with open('../handedness.txt') as f:
+                lines = f.readlines()
+                for l in lines:
+                    l = l.replace('\n', '')
+                    tmp = l.split(' ')
+                    handedness_lookup[tmp[0]] = tmp[1]
+        except IOError:
+            print('handedness.txt file not found')
+            print('please touch file and place ../[this script] or change pathing in this script')
+
+        return handedness_lookup
+
+    # Divided sample interval by 3 so sampled frames have at least a small degree of randomness to them
+    # but also aren't consecutive frames... This could be a bad idea but idk
+    def gen_sample(self, fps):
+        sample_range = (int(fps * self.start_time), int(fps * self.end_time))
+        sample_interval = (sample_range[1] - sample_range[0]) // int((self.num_cal_imgs * 3))
+        return set(sample(range(sample_range[0] + 1, sample_range[1] + 2, sample_interval), k=self.num_cal_imgs))  # +1 and +2 cause frame indices start on 1
+
+    def load_vid(self):
+        # Playing video from file:
+        cap = cv2.VideoCapture(self.project_path + os.sep + self.vid_name)
+
+        # get height and width from opencv and print to console
+        if cap.isOpened():
+            w = cap.get(3)  # 3 = cv2.CAP_PROP_FRAME_WIDTH
+            h = cap.get(4)  # 4 = cv2.CAP_PROP_FRAME_HEIGHT
+            fps = cap.get(5)  # 5 is FPS and returned 60 (correct)... but 7 is frame count and returned 1206fps for some reason
+            print('video height and width and fps: {}, {}, {}'.format(h, w, fps))
+
+        sample_frames = self.gen_sample(fps)
+
+        # output jpgs will be stored in [your project dir]/calibration_images
+        try:
+            if not os.path.exists(self.project_path + os.sep + 'calibration_images'):
+                os.makedirs(self.project_path + os.sep + 'calibration_images')
+        except OSError:
+            print('Error: Creating directory of data')
+
+        # TODO: find curr and check that this works
+        RFID = self.vid_name[:-4].split('_')[-3]
+
+        current_frame = 1
+        sample_frame_cnt = 1
+        while (True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+
+            # this logic limits cap to fps from video input, aka output will not have unecessarily high fps
+            if not ret:
+                break
+
+            gray = cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2GRAY)
+            if self.handedness_lookup[RFID] == 'L':
+                gray = cv2.flip(gray, 1)
+            # pixel coords of left cam
+            start_row, start_col = int(0), int(0)
+            end_row, end_col = int(h), int(w // 3 + 10)
+            cropped_left = gray[start_row:end_row, start_col:end_col]
+
+            # pixel coords of mid cam
+            start_row, start_col = int(0), int(w // 3 - 5)
+            end_row, end_col = int(h), int((w // 3) * 2 + 5)
+            cropped_mid = gray[start_row:end_row, start_col:end_col]
+
+            # pixel coords of right cam
+            # start_row, start_col = int(0), int((w // 3) * 2 - 10)
+            # end_row, end_col = int(h), int(w)
+            # cropped_right = gray[start_row:end_row, start_col:end_col]
+
+            # Saves image of the current frame in jpg file if frame # is in the sample
+
+            if current_frame in sample_frames:
+                name1 = self.project_path + os.sep + 'calibration_images' + os.sep + 'camera-1-' + str(sample_frame_cnt).zfill(3) + '.jpg'
+                cv2.imwrite(name1, cropped_left)
+
+                name2 = self.project_path + os.sep + 'calibration_images' + os.sep + 'camera-2-' + str(sample_frame_cnt).zfill(3) + '.jpg'
+                cv2.imwrite(name2, cropped_mid)
+                sample_frame_cnt += 1
+
+
+            # # print sth encouraging to console
+            # if current_frame % 10 == 0 and current_frame != 0:
+            #     print(str(current_frame / 70) + r'% done...')
+
+            # To stop duplicate images
+            current_frame += 1
+
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
+            
 
 if __name__ == '__main__':
     input_folder = 'raw_vids'
